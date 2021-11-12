e4dbad10c5d142c7f78cbd7800ad74350512ef45
JorgeUpGrade <91381135+JorgeUpGrade@users.noreply.github.com>
2021-11-12 18:58:59 +0100

    Lab 1

---
+++ lab.js
@@ -0,0 +1,12 @@
+let hacker1 = "Jorge"
+
+console.log(`The driver's name is ${hacker1}`)
+let hacker2 = "Johannes"
+console.log(`The navigator's name is ${hacker2}`)
+console.log(`It seems the navigator's has the longest name, it has ${hacker2.length} characters`)
+for (let i = 0; i <= hacker2.length; i++){const letter = hacker2[i];                                  console.log(letter.toUpperCase())}
+
+for (let i = hacker1.length - 1; i >= 0; i--){const letter1 = hacker1[i];
+console.log(letter1)};
+
+console.log("Yo, the navigator goes first definitely.");
